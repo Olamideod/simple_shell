af2ac78495f5d0423808f0a5b95cad6312628233
Olamideod <oluwamusiwaolamide@gmail.com>
2023-08-23 18:22:28 -0700

    shell.h

--- main.c
+++ main.c
@@ -1,59 +1,80 @@
 #include "shell.h"
 
-/**
- * main - Entry point for the simple shell program.
- *
- * Return: Always 0.
- */
-int main(void)
+/* Handles the interrupt signal by printing a new prompt */
+void sig_handler(int sig);
+
+/* Executes a command in a child process */
+int execute(char **args, char **front);
+
+/* Entry point for the simple shell program */
+int main(int argc, char *argv[])
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
 
-    while (1)
-    {
-        /* Display shell prompt */
-        printf("$ ");
-        /* Read user input */
-        nread = custom_getline(&line, &len, stdin);
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
 
-        if (nread == -1)
-        {
-            perror("getline error");
-            break;
-        }
-        if (nread == 1)
-            continue; /* Skip empty line */
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
 
-        line[nread - 1] = '\0'; /* Remove newline character */
+	/* Check for command-line arguments */
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
-        /* Tokenize input */
-        char **args = custom_tokenizer(line);
-        if (args == NULL)
-        {
-            fprintf(stderr, "Tokenizer error\n");
-            free(line);
-            continue;
-        }
+	/* Check if input is not from a terminal */
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
-        /* Execute command */
-        int exit_status = execute(args);
+	/* Interactive shell loop */
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
 
-        /* Free memory */
-        free(line);
-        for (int i = 0; args[i] != NULL; i++)
-            free(args[i]);
-        free(args);
-
-        if (exit_status == -1)
-        {
-            perror("Execution error");
-            break;
-        }
-    }
-
-    free(line);
-
-    return 0;
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
+
+/* Handles the interrupt signal by printing a new prompt */
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/* Executes a command in a child process */
+int execute(char **args, char **front)
+{
+	/* ... (rest of the function) ... */
+}
+
--- shell.h
+++ shell.h
@@ -11,30 +11,29 @@
 
 #define MAX_ARGUMENTS 32
 
-// Tokenizer function without using strtok
+/* Tokenizer function without using strtok*/
 char **tokenizer(const char *input);
 
-// Execute a command
+/* Execute a command*/
 int execute(char **input);
 
-// Built-in command handlers
+/* Built-in command handlers*/
 int shell_cd(char **args);
 int shell_exit(void);
 int shell_env(char **envp);
 int shell_setenv(char *args, char **envp);
 int shell_unsetenv(char *args, char **envp);
 
-// Advanced tasks functions
+/* Advanced tasks functions*/
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream);
 char **custom_tokenizer(const char *input);
 
 
 
-// Task-specific built-in command handlers
+/* Task-specific built-in command handlers*/
 int shell_alias(char **args);
 int shell_variables(char **args);
 int shell_file_input(char **args);
 
 #endif
- 
-  
+
