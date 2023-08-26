3cfe15050454febf1abca30c3b76a674842272e9
mega-mind01 <muyiwameleki@gmail.com>
2023-08-25 17:20:19 -0700

    debug 5

--- hist.c
+++ hist.c
@@ -72,31 +72,22 @@ int read_history(info_t *info)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	free(filename);
-
 	if (fd == -1)
 		return (0);
-
 	if (!fstat(fd, &st))
 		fsize = st.st_size;
-
 	if (fsize < 2)
 		return (0);
-
 	buf = malloc(sizeof(char) * (fsize + 1));
 	if (buf == NULL)
 		return (0);
-
 	rdlen = read(fd, buf, fsize);
 	buf[fsize] = '\0';
-
 	if (rdlen <= 0)
 		return ((free(buf), 0));
-
 	close(fd);
-
 	for (i = 0; i < fsize; i++)
 	{
 		if (buf[i] == '\n')
@@ -106,16 +97,12 @@ int read_history(info_t *info)
 			last = i + 1;
 		}
 	}
-
 	if (last != i)
 		build_history_list(info, buf + last, linecount++);
-
 	free(buf);
 	info->histcount = linecount;
-
 	while (info->histcount-- >= HIST_MAX)
 		delete_node_at_index(&(info->history), 0);
-
 	renumber_history(info);
 	return (info->histcount);
 }
